5d2e5830019291b48063f7ee3f2788fe613d4695
Jaydenzk <joowan88@gmail.com>
2020-05-21 00:38:39 -0700

    updated

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,14 +2,26 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+import collections
+
 def sliding_window_max(nums, k):
     # Your code here
+    dq = collections.deque()
+    result = []
+    for i in range(len(nums)):
+        while dq and nums[dq[-1]] <= nums[i]:
+            dq.pop()
+        dq.append(i)
+        if dq[0] == i - k:
+            dq.popleft()
+        if i >=k - 1:
+            result.append(nums[dq[0]])
+    return result
 
-    pass
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
